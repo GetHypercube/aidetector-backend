cc30f176b010850a923af8e638fd47f5e53439dd
Luis Cosio <luisalfonsocosioizcapa@gmail.com>
2023-11-24 10:22:43 -0600

    Moved logic to utils.py

--- dmdetector.py
+++ dmdetector.py
@@ -9,13 +9,12 @@ It prints out the logits returned by each model and the final label based on the
 import argparse
 import time
 import json
-import os
 import torch
-import psutil
 import numpy as np
-from PIL import Image, UnidentifiedImageError
+from PIL import Image
 import torchvision.transforms as transforms
 import networks.resnet_mod as resnet_mod
+from utils import compress_and_resize_image, print_memory_usage
 
 models_config = {
     "Grag2021_progan": {
@@ -31,45 +30,6 @@ models_config = {
         "patch_size": None,
     },
 }
-
-def compress_and_resize_image(image_path, max_size=(1024, 1024)):
-    """
-    Compresses and resizes an image to a manageable size.
-
-    Args:
-        image_path (str): Path to the image file.
-        max_size (tuple): Maximum width and height of the resized image.
-
-    Returns:
-        str: Path to the processed image.
-    """
-    try:
-        # Validate the file format
-        if not image_path.lower().endswith(('.png', '.jpg', '.jpeg', '.webp')):
-            raise ValueError("Unsupported file format. Accepts only JPEG, PNG, and WebP.")
-
-        # Open and process the image
-        with Image.open(image_path) as img:
-            if img.size[0] > max_size[0] or img.size[1] > max_size[1]:
-                # Resize the image only if it's larger than max_size
-                img.thumbnail(max_size, Image.LANCZOS)
-            # Save the processed image in a lossless format
-            processed_image_path = os.path.splitext(image_path)[0] + "_processed.png"
-            img.save(processed_image_path, format='PNG', optimize=True)
-            return processed_image_path
-
-    except UnidentifiedImageError as exc:
-        # Explicitly re-raising with context from the original exception
-        raise ValueError("Invalid image file or path.") from exc
-
-def print_memory_usage():
-    """
-    Prints the current memory usage of the process.
-    """
-    process = psutil.Process(os.getpid())
-    mem_info = process.memory_info()
-    print(f"Memory used: {mem_info.rss / (1024 * 1024):.2f} MB")
-
 
 def load_model(arch, model_path):
     """
--- gandetector.py
+++ gandetector.py
@@ -6,52 +6,14 @@ input image before feeding it to the models for prediction.
 It prints out the logits returned by each model and the final label based on these logits.
 """
 
-import os
 import time
 import argparse
 import json
-import psutil
 import torch
 import numpy as np
-from PIL import Image, UnidentifiedImageError
+from PIL import Image
 from networks.resnet50nodown import resnet50nodown
-
-def compress_and_resize_image(image_path, max_size=(1024, 1024)):
-    """
-    Compresses and resizes an image to a manageable size.
-
-    Args:
-        image_path (str): Path to the image file.
-        max_size (tuple): Maximum width and height of the resized image.
-
-    Returns:
-        str: Path to the processed image.
-    """
-    try:
-        # Validate the file format
-        if not image_path.lower().endswith(('.png', '.jpg', '.jpeg', '.webp')):
-            raise ValueError("Unsupported file format. Accepts only JPEG, PNG, and WebP.")
-
-        # Open and process the image
-        with Image.open(image_path) as img:
-            if img.size[0] > max_size[0] or img.size[1] > max_size[1]:
-                # Resize the image only if it's larger than max_size
-                img.thumbnail(max_size, Image.LANCZOS)
-            # Save the processed image in a lossless format
-            processed_image_path = os.path.splitext(image_path)[0] + "_processed.png"
-            img.save(processed_image_path, format='PNG', optimize=True)
-            return processed_image_path
-
-    except UnidentifiedImageError as exc:
-        # Explicitly re-raising with context from the original exception
-        raise ValueError("Invalid image file or path.") from exc
-
-def print_memory_usage():
-    """Prints the current memory usage of the process."""
-    process = psutil.Process(os.getpid())
-    mem_info = process.memory_info()
-    print(f"Memory used: {mem_info.rss / (1024 * 1024):.2f} MB")
-
+from utils import compress_and_resize_image, print_memory_usage
 
 models_config = {
     "gandetection_resnet50nodown_progan": {
