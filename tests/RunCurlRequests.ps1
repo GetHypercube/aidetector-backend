34b73c83f208d0b161c36b55d71294ce3ed4ed08
Luis Cosio <luisalfonsocosioizcapa@gmail.com>
2023-11-28 12:15:07 -0600

    Minor changes to the task definition to run un GPU instances

--- tests/RunCurlRequests.ps1
+++ tests/RunCurlRequests.ps1
@@ -1,8 +1,8 @@
 $scriptBlock = {
     param($i)
-    curl -X POST -F "file=@test.jpg" http://localhost:8080/detect -k
+    curl -X POST -F "file=@test.jpg" http://aidetector-prod-394527615.us-east-1.elb.amazonaws.com/detect -k
 }
 
-1..100 | ForEach-Object {
+1..1 | ForEach-Object {
     Start-Job -ScriptBlock $scriptBlock -ArgumentList $_
 } | Receive-Job -Wait -AutoRemoveJob
